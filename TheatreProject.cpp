e93592dd9257f760e64d4acd76dc1c1eda91955c
snichita817 <snichita817@gmail.com>
2022-03-13 10:26:31 +0200

    fixed bug where you can't read multiple spectacles

--- TheatreProject.cpp
+++ TheatreProject.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <cstring>
 using namespace std;
 
 class Actor{
@@ -8,9 +9,104 @@ class Actor{
     int age;
 public:
     friend istream& operator>>(istream&, Actor&);
-    friend ostream& operator<<(ostream&, Actor&);
+    friend ostream& operator<<(ostream&, const Actor&);
+    Actor() { this->name = "-"; this->age = 0; }
+    //~Actor() {
+    //    cout << this->name << " is no longer working in the theatre.";
+    //}
 };
 
+class Spectacle {
+    char* spectacle_name;
+    int number_of_actors;
+    vector<Actor> actors;
+    double rating;
+
+public:
+    friend istream& operator>>(istream&, Spectacle&);
+    friend ostream& operator<<(ostream&, const Spectacle&);
+    //Spectacle& operator=(Spectacle&);
+    //~Spectacle();
+};
+
+class Theatre {
+    char* address;
+    vector<Spectacle> spectacles;
+public:
+    void set_address(const char* s);
+    char* get_address();
+};
+
+int main()
+{
+    Spectacle s1, s2;
+    cin >> s1;
+    cout << s1;
+    cout << "================================"<<endl;
+    cin >> s2;
+    return 0;
+}
+
+//Theatre
+void Theatre::set_address(const char* input) {
+    this->address = new char[strlen(input) + 1];
+    strcpy(this->address, input);
+}
+
+char* Theatre::get_address() {
+    return address;
+}
+
+// Spectacle
+//Spectacle& Spectacle::operator= (Spectacle& spec) {
+//    if (this != &spec) {
+//        cout << "Copying spectacle details..." << endl;
+//        this->spectacle_name = spec.spectacle_name;
+//        this->number_of_actors = spec.number_of_actors;
+//        this->rating = spec.rating;
+//    }
+//    return *this;
+//}
+
+istream& operator>>(istream& in, Spectacle& spec) {
+    char s[255];
+    cout << "Enter the name of the play: ";
+    in.ignore();
+    in.get(s, 255);
+    spec.spectacle_name = new char[strlen(s) + 1];
+    strcpy(spec.spectacle_name, s);
+    in.get();
+    cout << "Enter the rating of the play: ";
+    in >> spec.rating;
+
+    cout << "Enter how many actors there will be: ";
+    in >> spec.number_of_actors;
+
+    for (int i = 0; i < spec.number_of_actors; i++) {
+        Actor temp;
+        in >> temp;
+        spec.actors.push_back(temp);
+    }
+
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Spectacle& spec) {
+    out << "The name of the spectacle: " << spec.spectacle_name << endl;
+    out << "Rating of the spectacle: " << spec.rating << endl;
+    out << "Number of actors " << spec.number_of_actors << endl;
+    for (int i = 0; i < spec.number_of_actors; i++) {
+        out << spec.actors[i] << endl;
+    }
+    return out;
+}
+
+//Spectacle::~Spectacle() {
+//    cout << this->spectacle_name << " is no longer in our theatres.";
+//}
+
+
+// Actor
 istream& operator>>(istream& in, Actor& act) {
     cout << "Insert the name of the actor: ";
     in >> act.name;
@@ -19,17 +115,8 @@ istream& operator>>(istream& in, Actor& act) {
     return in;
 }
 
-ostream& operator<<(ostream& out, Actor& act) {
+ostream& operator<<(ostream& out, const Actor& act) {
     out << "Actor's name is " << act.name << endl;
     out << "Actor's age is " << act.age << endl;
     return out;
-}
-
-int main()
-{
-    Actor test;
-    cin >> test;
-    cout << endl;
-    cout << test;
-    return 0;
 }
